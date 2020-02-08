b200aa288fab7edbd38778b213945f1b060c68df
BGDNick <49807419+BGDNick@users.noreply.github.com>
2020-02-08 11:42:01 +0300

    something added

--- Project5/Source.cpp
+++ Project5/Source.cpp
@@ -1,10 +1,16 @@
 #include <boost/any.hpp>
 #include <iostream>
 
+void f()
+{
+	std::cout << "working from VS" << std::endl;
+}
+
 int main()
 {
 	std::cout << "hello world " << std::endl;
-	int a = 2+2;
+	std::cout << "visual studio" << std::endl;
+
 	return 0;
 
 }
